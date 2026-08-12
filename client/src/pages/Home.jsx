8bc442ad74eb9dc8bea10e58aef830cbcd35141b
babluverse <bablu8602@xavier.edu.np>
2026-08-12 13:53:43 +0545

    test(frontend): preview ui primitives on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,21 @@
+import Button from '../components/Button';
+import Card from '../components/Card';
+import Skeleton from '../components/Skeleton';
+
 function Home() {
   return (
-    <div>
-      <h1>MediFlow</h1>
+    <div className="p-6 space-y-4">
+      <h1 className="text-2xl font-bold text-white">MediFlow</h1>
+
+      <Card>
+        <p className="text-white mb-3">Sample Card</p>
+        <Button variant="primary">Book Token</Button>
+      </Card>
+
+      <Card>
+        <Skeleton height="h-6" className="mb-2" />
+        <Skeleton height="h-4" width="w-2/3" />
+      </Card>
     </div>
   );
 }
